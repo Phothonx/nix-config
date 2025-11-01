07564030eee766610ed08505de3a8be3a2168905
Phothonx <86020708+Phothonx@users.noreply.github.com>
2025-11-01 23:57:35 +0100

    holy git config

--- home/nico/cli/git.nix
+++ home/nico/cli/git.nix
@@ -1,9 +1,24 @@
-{...}: {
+{config, ...}: {
+  sops.secrets."git_config/thor" = {
+    sopsFile = ../../../secrets/nico.yaml;
+    format = "yaml";
+    path = "${config.xdg.configHome}/git/thor_config";
+  };
+  sops.secrets."git_config/github" = {
+    sopsFile = ../../../secrets/nico.yaml;
+    format = "yaml";
+    path = "${config.xdg.configHome}/git/github_config";
+  };
+
   programs.git = {
     enable = true;
     settings = {
-      user.name = "Phothonx";
-      user.email = "86020708+Phothonx@users.noreply.github.com";
+      init.defaultBranch = "main";
+      commit.verbose = true;
+
+      "includeIf \"hasconfig:remote.*.url:git@github.com:*/**\"".path = config.sops.secrets."git_config/github".path;
+      "includeIf \"hasconfig:remote.*.url:git@thor.enseirb-matmeca.fr:*/**\"".path = config.sops.secrets."git_config/thor".path;
     };
   };
+
 }
