4278a2d56ffaf596ec725871c85892d5bd42a941
Phothonx <86020708+Phothonx@users.noreply.github.com>
2026-05-07 22:27:24 +0200

    camlann installed

--- modules/features/impermanence.nix
+++ modules/features/impermanence.nix
@@ -20,8 +20,8 @@
       ];
       files = [
         "/etc/machine-id"
-        "/etc/ssh/ssh_host_ed25519_key"
-        "/etc/ssh/ssh_host_ed25519_key.pub"
+        # "/etc/ssh/ssh_host_ed25519_key"
+        # "/etc/ssh/ssh_host_ed25519_key.pub"
       ];
     };
 
