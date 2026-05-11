ac4978332ac1988c289ca13be87e4559113fdc8e
Phothonx <86020708+Phothonx@users.noreply.github.com>
2026-05-11 22:49:28 +0200

    removed tls

--- modules/selfhosted/caddy.nix
+++ modules/selfhosted/caddy.nix
@@ -5,27 +5,21 @@
       openFirewall = true; # 443 (https) 80 (http)
       virtualHosts."immich.camlann.local".extraConfig = ''
         reverse_proxy localhost:2283
-        tls internal
       '';
       virtualHosts."paperless.camlann.local".extraConfig = ''
         reverse_proxy localhost:28981
-        tls internal
       '';
       virtualHosts."homepage.camlann.local".extraConfig = ''
         reverse_proxy localhost:8082
-        tls internal
       '';
       virtualHosts."adguard.camlann.local".extraConfig = ''
         reverse_proxy localhost:3000
-        tls internal
       '';
       virtualHosts."jellyfin.camlann.local".extraConfig = ''
         reverse_proxy localhost:8096
-        tls internal
       '';
       virtualHosts."qbit.camlann.local".extraConfig = ''
         reverse_proxy localhost:8080
-        tls internal
       '';
     };
   };
