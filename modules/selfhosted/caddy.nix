09fe400de90a3b7ebad030cd72fa9435e3f0b965
Phothonx <86020708+Phothonx@users.noreply.github.com>
2026-05-18 16:11:58 +0200

    cert

--- modules/selfhosted/caddy.nix
+++ modules/selfhosted/caddy.nix
@@ -1,15 +1,19 @@
 {
-  flake.nixosModules.caddy = {
+  flake.nixosModules.caddy = {pkgs, ...}: {
+    environment.systemPackages = [ pkgs.nsstools ];
     services.caddy = {
       enable = true;
       openFirewall = true; # 443 (https) 80 (http)
       virtualHosts."immich.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:2283
       '';
       virtualHosts."paperless.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:28981
       '';
       virtualHosts."homepage.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:8082
         handle /leaves.png {
           root * ${../../wallpapers}
@@ -17,27 +21,35 @@
         }
       '';
       virtualHosts."adguard.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:3000
       '';
       virtualHosts."jellyfin.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:8096
       '';
       virtualHosts."qbit.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:8080
       '';
       virtualHosts."radarr.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:7878
       '';
       virtualHosts."sonarr.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:8989
       '';
       virtualHosts."lidarr.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:8686
       '';
       virtualHosts."bazarr.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:6767
       '';
       virtualHosts."prowlarr.camlann.local".extraConfig = ''
+        tls internal
         reverse_proxy localhost:9696
       '';
     };
