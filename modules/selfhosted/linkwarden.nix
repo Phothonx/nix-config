b4bf01941dea850136051030ab1c08ef41715412
Phothonx <86020708+Phothonx@users.noreply.github.com>
2026-06-28 23:20:58 +0200

    linkwarden register

--- modules/selfhosted/linkwarden.nix
+++ modules/selfhosted/linkwarden.nix
@@ -1,9 +1,5 @@
 {
   flake.nixosModules.linkwarden = {config, ...}: {
-    # NEXTAUTH_SECRET (and any other env secrets) live here:
-    #   cd secrets && agenix -e linkwarden-env.age
-    # File contents, e.g.:
-    #   NEXTAUTH_SECRET=<openssl rand -base64 32>
     age.secrets.linkwarden-env = {
       file = ../../secrets/linkwarden-env.age;
       owner = "linkwarden";
@@ -13,8 +9,8 @@
       enable = true; # 3001 (3000 is taken by adguard)
       port = 3001;
       host = "127.0.0.1";
+      enableRegistration = true;
 
-      # local postgres, created automatically (shares the cluster with immich)
       database.createLocally = true;
 
       environmentFile = config.age.secrets.linkwarden-env.path;
