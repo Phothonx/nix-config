09f2a81867ae965d58905b2e6c8429b9c2fbd944
Phothonx <86020708+Phothonx@users.noreply.github.com>
2026-07-27 23:49:03 +0200

    beets update

--- modules/selfhosted/beets.nix
+++ modules/selfhosted/beets.nix
@@ -20,13 +20,10 @@
         write: yes
         log: /data/beets/import.log
 
-      plugins: fetchart embedart lyrics permissions musicbrainz discogs replaygain scrub ftintitle duplicates missing badfiles lastgenre
+      plugins: fetchart embedart lyrics permissions musicbrainz replaygain scrub ftintitle duplicates missing badfiles lastgenre
 
       musicbrainz:
           data_source_mismatch_penalty: 0.5
-
-      discogs:
-          data_source_mismatch_penalty: 0.0
 
       lyrics:
         synced: yes
